40962bd67a690245dbf962a3b1994d138acfcd53
sksmagr23 <sakshamag34@gmail.com>
2024-12-23 22:45:58 +0530

    [step4]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -19,24 +19,36 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
-	
+
 	filename := os.Args[2]
 	contents, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
-	
+
 	for _, char := range contents {
 		switch char {
 		case '(':
 			fmt.Println("LEFT_PAREN ( null")
 		case ')':
 			fmt.Println("RIGHT_PAREN ) null")
-	    case '{':
+		case '{':
 			fmt.Println("LEFT_BRACE { null")
 		case '}':
-			fmt.Println("RIGHT_BRACE } null")		
+			fmt.Println("RIGHT_BRACE } null")
+		case ',':
+			fmt.Println("COMMA , null")
+		case '.':
+			fmt.Println("DOT . null")
+		case '-':
+			fmt.Println("MINUS - null")
+		case '+':
+			fmt.Println("PLUS + null")
+		case ';':
+			fmt.Println("SEMICOLON ; null")
+		case '*':
+			fmt.Println("STAR * null")
 		}
 	}
 	fmt.Println("EOF  null")
