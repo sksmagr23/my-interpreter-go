5a6b5da30a255e701d73f3505d0ae1cfbdbd3e2c
sksmagr23 <sakshamag34@gmail.com>
2024-12-23 22:22:56 +0530

    [step1]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -20,19 +20,17 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
-
-	// Uncomment this block to pass the first stage
-	//
-	// filename := os.Args[2]
-	// fileContents, err := os.ReadFile(filename)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// if len(fileContents) > 0 {
-	// 	panic("Scanner not implemented")
-	// } else {
-	// 	fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
-	// }
+	
+	filename := os.Args[2]
+	fileContents, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+	
+	if len(fileContents) > 0 {
+		panic("Scanner not implemented")
+	} else {
+		fmt.Println("EOF  null")
+	}
 }
